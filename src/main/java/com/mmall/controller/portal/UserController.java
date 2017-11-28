6e58f3100e03f293171c67497de1ac019144d36a
Xie zhongtian <xiezhongtian@emindsoft.com.cn>
2017-11-28 11:42:43 +0800

    add getInformation adminLogin PropertiesUtil operation

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,6 +1,7 @@
 package com.mmall.controller.portal;
 
 import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
@@ -42,14 +43,14 @@ public class UserController {
         return response;
     }
 
-    @RequestMapping(value = "logout.do",method = RequestMethod.GET)//limit request as GET
+    @RequestMapping(value = "logout.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> logout(HttpSession session){
         session.removeAttribute(Const.CURRENT_USER);
         return ServerResponse.createBySuccess();
     }
 
-    @RequestMapping(value = "register.do",method = RequestMethod.POST)//limit request as GET
+    @RequestMapping(value = "register.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> register(User user){
         return iUserService.register(user);
@@ -63,7 +64,7 @@ public class UserController {
      * @Return:
      * @Date:   17-11-24 下午4:30
      */
-    @RequestMapping(value = "check_valid.do",method = RequestMethod.GET)//limit request as GET
+    @RequestMapping(value = "check_valid.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> checkValid(String str, String type){
         return iUserService.checkValid(str,type);
@@ -76,7 +77,7 @@ public class UserController {
      * @Return: 
      * @Date:   17-11-24 下午4:56
      */
-    @RequestMapping(value = "get_user_info.do",method = RequestMethod.GET)//limit request as GET
+    @RequestMapping(value = "get_user_info.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> getUserInfo(HttpSession session){
         User user = (User) session.getAttribute(Const.CURRENT_USER);
@@ -87,27 +88,27 @@ public class UserController {
     }
 
     //忘记密码
-    @RequestMapping(value = "forget_get_question.do",method = RequestMethod.GET)//limit request as GET
+    @RequestMapping(value = "forget_get_question.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> forgetGetQuestion(String username){
         return iUserService.selectQuestion(username);
     }
 
     //校验问题答案
-    @RequestMapping(value = "forget_check_answer.do",method = RequestMethod.GET)//limit request as GET
+    @RequestMapping(value = "forget_check_answer.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String>forgetCheckAnswer(String username, String question, String answer){
         return iUserService.checkAnswer(username, question, answer);
     }
 
     //忘记密码重置密码
-    @RequestMapping(value = "forget_reset_password.do",method = RequestMethod.GET)//limit request as GET
+    @RequestMapping(value = "forget_reset_password.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> forgetRestPassword(String username, String passwordNew, String forgetToken){
         return iUserService.forgetResetPassword(username, passwordNew, forgetToken);
     }
 
-    @RequestMapping(value = "reset_password.do",method = RequestMethod.GET)
+    @RequestMapping(value = "reset_password.do",method = RequestMethod.POST)
     @ResponseBody
     //重置密码
     public ServerResponse<String> resetPassword(HttpSession session, String passwordOld, String passwordNew){
@@ -118,12 +119,12 @@ public class UserController {
         return iUserService.resetPassword(passwordOld,passwordNew,user);
     }
 
-    @RequestMapping(value = "update_information.do",method = RequestMethod.GET)
+    @RequestMapping(value = "update_information.do",method = RequestMethod.POST)
     @ResponseBody
     //更新用户个人信息
     public ServerResponse<User> updateInformation(HttpSession session, User user){
         User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
-        if(user == null){
+        if(currentUser == null){
             return ServerResponse.createByErrorMessage("用户未登录");
         }
         //赋值为当前登录的userId和username
@@ -131,9 +132,19 @@ public class UserController {
         user.setUsername(currentUser.getUsername());
         ServerResponse<User> response = iUserService.updateInformation(user);
         if(response.isSuccess()){
-            response.getData().setUsername(currentUser.getUsername());
+            //response.getData().setUsername(currentUser.getUsername());
             session.setAttribute(Const.CURRENT_USER,response.getData());
         }
         return response;
+    }
+    @RequestMapping(value = "get_information.do",method = RequestMethod.POST)
+    @ResponseBody
+    //获取用户个人信息
+    public ServerResponse<User> getInformation(HttpSession session){
+        User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
+        if(currentUser == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，需要强制登录status=10");
+        }
+        return iUserService.getInformation(currentUser.getId());
     }
 }
---
+++ src/main/java/com/mmall/util/PropertiesUtil.java
@@ -0,0 +1,46 @@
+package com.mmall.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+/**
+ * @author: zhongtianemail@gmail.com
+ * @create 2017-11-28 上午11:06
+ * @Description:
+ **/
+public class PropertiesUtil {
+
+    private static org.slf4j.Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
+
+    private static Properties props;
+
+    static {
+        String fileName = "mmall.properties";
+        props = new Properties();
+        try {
+            props.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream(fileName),"UTF-8"));
+        }catch (IOException e){
+            logger.error("配置文件读取异常", e);
+        }
+    }
+
+    public static String getProperty(String key){
+        String value = props.getProperty(key);
+        if(StringUtils.isBlank(value)){
+            return null;
+        }
+        return value.trim();
+    }
+
+    public static String getProperty(String key,String defaultKey){
+        String value = props.getProperty(key);
+        if (StringUtils.isBlank(value)){
+            return defaultKey;
+        }
+        return value.trim();
+    }
+}
